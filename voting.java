aaea074c70cafb030f205bfe5684b58bc1f12cdf
venkateswarulu862 <kasinenivenkateswarulu@gmail.com>
2019-09-20 22:53:32 -0700

    two

---
+++ voting.java
@@ -0,0 +1,29 @@
+public class Vote
+
+{
+
+public static void main(String args[])
+
+{
+
+int a=22;//take input age
+
+if(a>18)//condition for eligble to vote
+
+{
+
+System.out.println("eligble to Vote");
+
+}
+
+else
+
+{
+
+System.out.println("not eligble to Vote");
+
+}
+
+}
+
+}
